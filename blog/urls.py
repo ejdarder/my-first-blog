147717ccf7cb875cb07ea9e4df1c718e17a1fe51
Elena Jordana <elena.jdarder@gmail.com>
2020-04-28 18:04:03 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,7 @@
-# import Django's function path
 from django.urls import path
-# import all views from the blog application
 from . import views
 
-# add first URL pattern
 urlpatterns = [
-	path('', views.post_list, name='post_list'),
-] # we just assigned a view called post_list to the root URL
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+]
